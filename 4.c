b95aa909e206f5dccb52cf38c4f6228302a0c34a
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-06-21 10:29:41 +0530

    LeetCode - 4

---
+++ 4.c
@@ -0,0 +1,41 @@
+// 4. Median of Two Sorted Arrays
+
+#include <stdio.h>
+#include <limits.h>
+
+double findMedianSortedArray(int* nums1, int nums1Size, int* nums2, int nums2Size){
+    if (nums1Size > nums2Size){
+        return findMedianSortedArray(nums2, nums2Size, nums1, nums1Size);
+    }
+
+    int m = nums1Size;
+    int n = nums2Size;
+
+    int l = 0, r = m;
+
+    while (l <= r){
+        int partitionX = (l + r) / 2;
+        int partitionY = (m + n + 1) / 2 - partitionX;
+
+        int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+        int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
+
+        int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+        int minRighty = (partitionY == n) ? INT_MAX : nums2[partitionY];
+
+        if (maxLeftX <= minRighty && maxLeftY <= minRightX){
+            if ((m + n) % 2 == 0){
+                int leftMax = (maxLeftX > maxLeftY) ? maxLeftX : maxLeftY;
+                int rightMin = (minRightX < minRighty) ? minRightX : minRighty;
+                return (leftMax + rightMin) / 2.0;
+            }else{
+                return (double)((maxLeftX > maxLeftY) ? maxLeftX : maxLeftY);
+            }
+        }else if(maxLeftX > maxLeftY){
+            r = partitionX - 1;
+        }else{
+            l = partitionY + 1;
+        }
+    }
+    return 0.0;
+}
