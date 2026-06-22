822d1fafaa4171771b05050923733043767854f4
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-06-22 17:09:01 +0530

    LeetCode - 724

---
+++ 724.c
@@ -0,0 +1,20 @@
+// 724. Find Pivot Element
+
+int pivotIndex(int* nums, int numsSize) {
+    int total_sum = 0;
+    for (int i = 0; i < numsSize; i++){
+        total_sum += nums[i];
+    }
+
+    int right_sum = total_sum;
+    int left_sum = 0;
+
+    for (int i = 0; i < numsSize; i++){
+        right_sum -= nums[i];
+        if (left_sum == right_sum){
+            return i;
+        }
+        left_sum += nums[i];
+    }
+    return -1;
+}
