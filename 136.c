c76f43d2e0cb157af45c92ef16f479358d10ef78
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-06-25 18:58:09 +0530

    LeetCode - 58

---
+++ 136.c
@@ -0,0 +1,10 @@
+// 136. Single Number
+
+int singleNumber(int* nums, int numsSize) {
+    int result = 0;
+
+    for (int i = 0; i < numsSize; i++){
+        result = result ^ nums[i];
+    }
+    return result;
+}
