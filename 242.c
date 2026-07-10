f2e8e81576110fc233d4486ea1c6a58cca7671fb
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-07-10 19:04:20 +0530

    LeetCode - 242

---
+++ 242.c
@@ -0,0 +1,25 @@
+// 242. Valid Anagram
+
+#include <stdbool.h>
+
+bool isAnagram(char* s, char* t) {
+    int count[26] = {0};
+
+    while (*s) {
+        count[*s - 'a']++;
+        s++;
+    }
+
+    while (*t) {
+        count[*t - 'a']--;
+        t++;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (count[i] != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
