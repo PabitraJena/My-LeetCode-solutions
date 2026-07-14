604f6e327fb7578cf5350ae072a5ce1d09c0a6a5
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-07-14 22:05:13 +0530

    LeetCode - 3945

---
+++ 3945.c
@@ -0,0 +1,12 @@
+// 3945. Digit Frequency Score
+
+int digitFrequencyScore(int n) {
+    int score = 0;
+
+    while (n > 0) {
+        score += n % 10;
+        n /= 10;
+    }
+
+    return score;
+}
