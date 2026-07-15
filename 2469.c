1b16f53fa47f55739f310eb528d296b1ec0a91d5
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-07-15 19:19:24 +0530

    LeetCode - 2469

---
+++ 2469.c
@@ -0,0 +1,12 @@
+// 2469. Convert the temperature
+
+double* convertTemperature(double celsius, int* returnSize) {
+    *returnSize = 2;
+
+    double* ans = (double*)malloc(2 * sizeof(double));
+
+    ans[0] = celsius + 273.15;
+    ans[1] = celsius * 1.80 + 32.0;
+
+    return ans;
+}
