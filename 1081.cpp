4cabe2c47ea411f9abf9eab4e3fa1a911c8424fe
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-07-19 20:47:58 +0530

    LeetCode - 1081

---
+++ 1081.cpp
@@ -0,0 +1,38 @@
+// 1081. Smallest subsequence of distict characters
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> last(26);
+        
+        for (int i = 0; i < s.size(); i++)
+            last[s[i] - 'a'] = i;
+
+        vector<bool> inStack(26, false);
+        string st;
+
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+
+            if (inStack[c - 'a'])
+                continue;
+
+            while (!st.empty() && 
+                st.back() > c &&
+                last[st.back() - 'a'] > i) {
+                inStack[st.back() - 'a'] = false;
+                st.pop_back();
+            }
+
+            st.push_back(c);
+            inStack[c - 'a'] = true;
+        }
+
+        return st;
+    }
+};
