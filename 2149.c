e2df03724ccecd64034704f67cddb53f246ffafb
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-08-05 21:47:01 +0530

    LeetCode - 2149

---
+++ 2149.c
@@ -0,0 +1,22 @@
+// 2149. Rearrange Array Elements by Sign
+#include <stdlib.h>
+
+int* rearrangeArray(int* nums, int numsSize, int* returnSize) {
+    int *ans = (int *)malloc(numsSize * sizeof(int));
+
+    int pos = 0;
+    int neg = 1;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] > 0) {
+            ans[pos] = nums[i];
+            pos += 2;
+        } else {
+            ans[neg] = nums[i];
+            neg += 2;
+        }
+    }
+
+    *returnSize = numsSize;
+    return ans;
+}
