4c4be5edb0a5e05414e2baefa00da9fbee6a799f
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-08-08 20:43:55 +0530

    LeetCode - 1512

---
+++ 1512.c
@@ -0,0 +1,13 @@
+// 1512. Number of Good Pairs
+
+int numIdenticalPairs(int* nums, int numsSize) {
+    int count[101] = {0};
+    int ans = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        ans += count[nums[i]];
+        count[nums[i]]++;
+    }
+
+    return ans;
+}
