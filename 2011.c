052397f934aa130785c3bbb46889f80cf10fa489
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-08-09 20:42:57 +0530

    LeetCode - 2011

---
+++ 2011.c
@@ -0,0 +1,14 @@
+// 2011. Final Value of Variable After Performing Operations
+
+int finalValueAfterOperations(char** operations, int operationsSize) {
+    int x = 0;
+
+    for (int i = 0; i < operationsSize; i++) {
+        if (operations[i][1] == '+')
+            x++;
+        else
+            x--;
+    }
+
+    return x;
+}
