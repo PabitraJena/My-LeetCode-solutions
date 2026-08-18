20e2e43a35f029da7968503c4c1dc8d4ad396ff0
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-08-18 20:49:18 +0530

    LeetCode - 3471

---
+++ 3471.c
@@ -0,0 +1,46 @@
+// 3471. Find the Largest Almost Missing Integer
+
+#include <stdio.h>
+
+int largestInteger(int* nums, int numsSize, int k) {
+    int count[51] = {0};
+    int i;
+    
+    for (i = 0; i < numsSize; i++) {
+        count[nums[i]]++;
+    }
+
+    if (k == 1) {
+        for (i = 50; i >= 0; i--) {
+            if (count[i] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    if (k == numsSize) {
+        int max = nums[0];
+
+        for (i = 1; i < numsSize; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        return max;
+    }
+
+    int ans = -1;
+
+    if (count[nums[0]] == 1) {
+        ans = nums[0];
+    }
+
+    if (count[nums[numsSize - 1]] == 1 &&
+        nums[numsSize - 1] > ans) {
+        ans = nums[numsSize - 1];
+    }
+
+    return ans;
+}
