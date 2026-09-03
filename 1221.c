748460df78b3bce103157f4ba79ac864daf1424d
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-09-03 23:08:09 +0530

    LeetCode - 1221

---
+++ 1221.c
@@ -0,0 +1,18 @@
+// 1221. Split a String in Balanced Strings
+
+int balancedStringSplit(char* s){
+    int balance = 0;
+    int count = 0;
+
+    for (int i = 0; s[i] != '\0'; i++){
+        if (s[i] == 'L')
+            balance++;
+        else
+            balance--;
+
+        if (balance == 0)
+            count++;
+    }
+
+    return count;
+}
