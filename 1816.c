1906dc9c80d513974bbb8fee3c2e410e4170632c
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-09-20 22:40:34 +0530

    LeetCode - 1816

---
+++ 1816.c
@@ -0,0 +1,16 @@
+// 1816. Truncate Sentence
+
+char* truncateSentence(char* s, int k) {
+    int count = 0;
+
+    for (int i = 0; s[i] != '\0'; i++){
+        if (s[i] == ' '){
+            count++;
+            if (count == k){
+                s[i] = '\0';
+                break;
+            }
+        }
+    }
+    return s;
+}
