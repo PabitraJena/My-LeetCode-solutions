1f7a6c9d49d75c2c541fe0ab8365f2b97048259f
Pabitra Jena <235420101+PabitraJena@users.noreply.github.com.>
2026-09-24 20:49:21 +0530

    LeetCode - 3550

---
+++ 3550.c
@@ -0,0 +1,18 @@
+// 3550. Smallest Index With Digit Sum Equal to Index
+
+int smallestIndex(int* nums, int numsSize){
+    for (int i = 0; i < numsSize; i++){
+        int n = nums[i];
+        int sum = 0;
+
+        while (n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+
+        if (sum == i){
+            return i;
+        }
+    }
+    return -1;
+}
